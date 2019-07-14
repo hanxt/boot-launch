3ffb7658bb6cda3144a9001271f164ce9dfb4a95
hanxt <hanxiaotongtong@163.com>
2019-07-14 22:08:36 +0800

    1-3hello world

---
+++ src/main/java/com/zimug/bootlaunch/BootLaunchApplication.java
@@ -0,0 +1,13 @@
+package com.zimug.bootlaunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLaunchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLaunchApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/zimug/bootlaunch/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.zimug.bootlaunch.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+
+}
