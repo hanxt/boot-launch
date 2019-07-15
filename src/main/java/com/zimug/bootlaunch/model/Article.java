3c9e30c713e88ac6b3b82bf41252ff2d87ff77ba
hanxt <hanxiaotongtong@163.com>
2019-07-16 05:37:16 +0800

    2-3 JSON数据处理与Postman测试

--- src/main/java/com/zimug/bootlaunch/model/Article.java
+++ src/main/java/com/zimug/bootlaunch/model/Article.java
@@ -1,6 +1,7 @@
 package com.zimug.bootlaunch.model;
 
 
+import com.fasterxml.jackson.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,13 +14,19 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonPropertyOrder(value={"content","title"})
 public class Article {
 
-
+   @JsonIgnore
     private Long id;
+
+    @JsonProperty("auther")
     private String author;
     private String title;
     private String content;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
     private List<Reader> reader;
 
