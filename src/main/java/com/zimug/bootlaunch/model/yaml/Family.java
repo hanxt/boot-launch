5f93b35aaa1cf47ab9b24bb74a0c8dcffb7f4c06
hanxt <hanxiaotongtong@163.com>
2019-07-17 12:34:46 +0800

    3-4 配置文件注入值数据校验

--- src/main/java/com/zimug/bootlaunch/model/yaml/Family.java
+++ src/main/java/com/zimug/bootlaunch/model/yaml/Family.java
@@ -4,13 +4,18 @@ package com.zimug.bootlaunch.model.yaml;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @Component
+@Validated
 @ConfigurationProperties(prefix = "family")
 public class Family {
 
     //@Value("${family.family-name}")
+    @NotEmpty
     private String familyName;
 
     private Father father;
