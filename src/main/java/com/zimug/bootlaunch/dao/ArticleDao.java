f3e95e75ed2d95b15d201a289a89309b91f99698
hanxt <hanxiaotongtong@163.com>
2019-07-21 11:20:53 +0800

    4.15.一行代码实现RESTFul接口

--- src/main/java/com/zimug/bootlaunch/dao/ArticleDao.java
+++ src/main/java/com/zimug/bootlaunch/dao/ArticleDao.java
@@ -2,7 +2,10 @@ package com.zimug.bootlaunch.dao;
 
 import com.zimug.bootlaunch.model.Article;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource(collectionResourceRel = "article",path="articles")
 public interface ArticleDao extends MongoRepository<Article,String> {
         //支持关键字查询，和JPA的用法一样
         Article findByAuthor(String author);
