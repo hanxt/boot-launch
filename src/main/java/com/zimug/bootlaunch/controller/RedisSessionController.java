7ef2d475efb85d1e7a83464ed0c0acef1872f809
hanxt <hanxiaotongtong@163.com>
2019-07-27 18:30:03 +0800

    11.8.集群多节点应用session共享

---
+++ src/main/java/com/zimug/bootlaunch/controller/RedisSessionController.java
@@ -0,0 +1,25 @@
+package com.zimug.bootlaunch.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
+@RestController
+public class RedisSessionController {
+
+    @RequestMapping(value="/uid",method = RequestMethod.GET)
+    public @ResponseBody
+    String uid(HttpSession session) {
+        UUID uid = (UUID) session.getAttribute("uid");
+        if (uid == null) {
+            uid = UUID.randomUUID();
+        }
+        session.setAttribute("uid", uid);
+        return session.getId();
+    }
+}
--- src/test/java/com/zimug/bootlaunch/BootLaunchApplicationTests.java
+++ src/test/java/com/zimug/bootlaunch/BootLaunchApplicationTests.java
@@ -3,10 +3,12 @@ package com.zimug.bootlaunch;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 30 * 60 * 1000)
 public class BootLaunchApplicationTests {
 
     @Test
