0189e656cbcaf75736eeec643e0b4c9f985752b7
hanxt <hanxiaotongtong@163.com>
2019-07-27 09:30:33 +0800

    11.7.个性化自定义缓存到期时间

--- src/main/java/com/zimug/bootlaunch/config/RedisConfig.java
+++ src/main/java/com/zimug/bootlaunch/config/RedisConfig.java
@@ -3,6 +3,8 @@ package com.zimug.bootlaunch.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -13,8 +15,16 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
 @Configuration
+@Component
+@ConfigurationProperties(prefix = "caching")
 public class RedisConfig {
 
     @Bean
@@ -40,12 +50,39 @@ public class RedisConfig {
     }
 
 
-    //本节的重点配置，让缓存的序列化方式使用redisTemplate.getValueSerializer()
+    //自定义redisCacheManager
     @Bean
     public RedisCacheManager redisCacheManager(RedisTemplate redisTemplate) {
         RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisTemplate.getConnectionFactory());
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()));
-        return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+
+        RedisCacheManager redisCacheManager = new RedisCacheManager(redisCacheWriter,
+                this.buildRedisCacheConfigurationWithTTL(redisTemplate,RedisCacheConfiguration.defaultCacheConfig().getTtl().getSeconds()),  //默认的redis缓存配置
+                this.getRedisCacheConfigurationMap(redisTemplate)); //针对每一个cache做个性化缓存配置
+
+        return  redisCacheManager;
+    }
+
+    //配置注入，key是缓存名称，value是缓存有效期
+    private Map<String,Long> ttlmap;
+
+    //根据ttlmap的属性装配结果，个性化RedisCacheConfiguration
+    private Map<String, RedisCacheConfiguration> getRedisCacheConfigurationMap(RedisTemplate redisTemplate) {
+        Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
+
+        for(Map.Entry<String, Long> entry : ttlmap.entrySet()){
+            String cacheName = entry.getKey();
+            Long ttl = entry.getValue();
+            redisCacheConfigurationMap.put(cacheName,this.buildRedisCacheConfigurationWithTTL(redisTemplate,ttl));
+        }
+
+        return redisCacheConfigurationMap;
+    }
+
+
+    //让缓存的序列化方式使用redisTemplate.getValueSerializer()，并未每一个缓存分别设置ttl
+    private RedisCacheConfiguration buildRedisCacheConfigurationWithTTL(RedisTemplate redisTemplate,Long ttl){
+        return  RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()))
+                .entryTtl(Duration.ofSeconds(ttl));
     }
 }
